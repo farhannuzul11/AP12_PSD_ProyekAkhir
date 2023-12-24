27cc2f2f0955792ca6553cc774d984c0d99f344a
Nuzul <farhannuzul11@gmail.com>
2023-12-24 21:21:41 +0700

    commit

--- Memory.vhd
+++ Memory.vhd
@@ -7,8 +7,8 @@ package Memory is
     Type room IS RECORD
         status : STD_LOGIC;
         username : STRING(1 TO 11);
-        password : IN INTEGER;
-        day : IN INTEGER;
+        password : INTEGER;
+        day : INTEGER;
     END RECORD;
     --parking_lot = all_room, kapasitasnya berbeda 3 lantai masing2 4 kamar
     TYPE all_room IS ARRAY (2 DOWNTO 0, 3 DOWNTO 0) OF room;
